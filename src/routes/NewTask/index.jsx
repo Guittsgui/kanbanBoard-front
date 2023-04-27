c68626b5328e8a01184eaa489d32be4fab161c59
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-04-27 17:15:51 -0300

    dados dos campos sendo manipulados

--- src/routes/NewTask/index.jsx
+++ src/routes/NewTask/index.jsx
@@ -1,17 +1,58 @@
 import * as S from './style'
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 const NewTask = () => {
+    const data = {
+        title: '',
+        body: '',
+        type: 'doing'
+    }
+
+
+
+    const [inputs, setInputs] = useState(data)
+    const navigate = useNavigate()
 
     function handleSubmit(e){
         e.preventDefault()
-        alert('adicionou tarefa')
+        if(!inputs.body || !inputs.title){
+            return
+        }
+        const date = new Date()
+        const task = {...inputs, date: date}
+        
+        /// Adicionar new Task ao Banco.
+
+        /// Redirecionar para a página Inicial.
+
+        navigate("/")
+    }
+    function handleChange(e){
+        const {name,value} = e.target
+        setInputs((prev) =>{
+            const newData = {...prev, [name]: value}
+            return newData
+        })
+        
     }
 
     return <>
      <S.container>
         <h1> Adicionando New Task </h1>
-        <input type="text" placeholder='Título da Tarefa: '/>
-        <textarea placeholder='Descrição da tarefa:'/>
+        <input type="text" 
+        placeholder='Título da Tarefa:' 
+        value={inputs.title}
+        onChange={handleChange}
+        name="title"/>
+
+        <textarea placeholder='Descrição da tarefa:' 
+        value={inputs.body}
+        onChange={handleChange}
+        name="body"/>
+
         <button onClick={handleSubmit}>
             <p> Add Task</p>
         </button>
--- src/routes/SeeOneTask/index.jsx
+++ src/routes/SeeOneTask/index.jsx
@@ -1,8 +1,9 @@
 import * as S from './style'
+
 const SeeOneTask = ()=>{
 
     return <S.container>
-        
+
     </S.container>
 
 }
